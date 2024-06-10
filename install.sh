0aa4f751d3cf70ff5c0d2ff88d93c3eb33381ba1
dontuo <bench.oleg@gmail.com>
2024-06-10 11:12:40 +0300

    major cleaning files and update config

--- install.sh
+++ install.sh
@@ -3,3 +3,13 @@
 # Install packages
 sudo pacman -S --noconfirm --needed - <pacman.txt
 
+# move dot-files to .config folder
+cp .config/* ~/.config/
+cp .bashrc ~
+#installing yay
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+
+
