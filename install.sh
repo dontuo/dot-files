583556c46e35004482857b3be1833be617779928
dontuo <bench.oleg@gmail.com>
2024-06-12 00:34:02 +0300

    update the config. Added waypaper to config(very cool thing)

--- install.sh
+++ install.sh
@@ -3,13 +3,17 @@
 # Install packages
 sudo pacman -S --noconfirm --needed - <pacman.txt
 
+mkdir ~/Pictures
 # move dot-files to .config folder
 cp .config/* ~/.config/
 cp .bashrc ~
+cp -r wallpapers/ ~/Pictures/
 #installing yay
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
+
+yay -S --noconfirm --needed - <yay.txt 
 
 
