91a04c815caa8feec5690f5ed9d085a9b96b8d72
dontuo <bench.oleg@gmail.com>
2024-06-13 12:38:10 +0000

    update and fixed (maybe)

--- install.sh
+++ install.sh
@@ -3,17 +3,21 @@
 # Install packages
 sudo pacman -S --noconfirm --needed - <pacman.txt
 
+mkdir .config/
 mkdir ~/Pictures
 # move dot-files to .config folder
-cp .config/* ~/.config/
+cp -r .config/* ~/.config/
 cp .bashrc ~
 cp -r wallpapers/ ~/Pictures/
+
+
 #installing yay
-sudo pacman -S --needed git base-devel
+sudo pacman -S --needed htop btop blueman bluez-utils thunar kitty lazygit neofetch neovim qbittorrent ttf-font-awesome ttf-terminus-nerd vlc pulseaudio pipewire git xdg-desktop-portal-hyprland-git wofi pipewire  grim slurp pipewire-audio pipewire-pulse wireplumber wl-clipboard hyprland waybar zerotier-one wayvnc neovide pavucontrol firefox
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 
-yay -S --noconfirm --needed - <yay.txt 
+yay -S --noconfirm --needed wlogout swww waypaper
+ 
 
 
