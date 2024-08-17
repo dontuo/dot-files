8ac79913c877382d1507b0906171d7b437af39c5
dontuo <bench.oleg@gmail.com>
2024-08-18 01:23:50 +0300

    update the config and fix them

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # Install packages
 sudo pacman -S --noconfirm --needed - <pacman.txt
 
-mkdir .config/
+mkdir ~/.config/
 mkdir ~/Pictures
 # move dot-files to .config folder
 cp -r .config/* ~/.config/
@@ -15,12 +15,13 @@ cp -r wallpapers/ ~/Pictures/
 
 #installing yay
 
-pacman -Rsu $(comm -23 <(pacman -Qq | sort) <(sort pacman.txt))
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 
-yay -S --noconfirm --needed wlogout swww waypaper
+#installing packages
+yay -Rsu $(comm -23 <(yay -Qq | sort) <(sort pacman.txt))
+pacman -Rsu $(comm -23 <(pacman -Qq | sort) <(sort pacman.txt))
  
 
 
