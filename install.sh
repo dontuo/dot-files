6c8261669d4a9043eb9a061b98250000616e3ff9
dontuo <bench.oleg@gmail.com>
2024-08-18 11:30:35 +0300

    update config file

--- install.sh
+++ install.sh
@@ -18,14 +18,17 @@ cp -r wallpapers/ ~/Pictures/
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
-
+cd ..
 #installing packages
 sudo pacman -Syu --noconfirm
 
-sudo pacman -S --noconfirm --needed - <pacman.txt
-yay -S --noconfirm - <yay.txt
+#sudo pacman -S --noconfirm --needed - <pacman.txt
+#yay -S --noconfirm - <yay.txt
 
 # Install packages from the list
+sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
+sudo yay -S --needed $(comm -12 <(yay -Slq | sort) <(sort pkglist.txt))
+
 #yay -Rsu $(comm -23 <(yay -Qq | sort) <(sort pacman.txt))
 #pacman -Rsu $(comm -23 <(pacman -Qq | sort) <(sort pacman.txt))
  
